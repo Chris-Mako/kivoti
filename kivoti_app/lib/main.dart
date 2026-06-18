5118f9dd2078bbd14f886c8bf35aa1f92feac1b7
Chris Mako <chris.makowski@gmx.net>
2026-06-18 22:28:48 +0200

    modified main and added own dashbaord_screen

--- kivoti_app/lib/main.dart
+++ kivoti_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kivoti_app/screens/dashboard_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,8 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        home: DashboardScreen(),    );
   }
 }
 
